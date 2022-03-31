a3613433c6e68e5d9929b643152b9d9f3552135d
chendon@bpcs.com <chendon@bpcs.com>
2022-03-31 20:16:31 +0000

    mardown format edits

--- wake_and_cache.py
+++ wake_and_cache.py
@@ -1,17 +1,23 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC 
-# MAGIC #Purpose
+# MAGIC ##Purpose
 # MAGIC This notebook is a simple pattern to "wake up" a Databricks SQL endpoint and prepare it for a large load.
 # MAGIC The common usecase we find with many clients is that they have a large set of reporting to power through on say Monday mornings.
 # MAGIC 
 # MAGIC This script will simply start up the endpoint (before your reports) and cache some key tables to enable better performance when the reports do start.
 # MAGIC 
-# MAGIC NOTE: This caching process may not **necessarily** scale the endpoint up, which would also greatly increase reporting performance if done beforehand.
+# MAGIC #####NOTE: 
 # MAGIC 
-# MAGIC NOTE2a: A [Databricks Personal Access Token](https://docs.databricks.com/dev-tools/api/latest/authentication.html#:~:text=Generate%20a%20personal%20access%20token,-This%20section%20describes&text=Settings%20in%20the%20lower%20left,the%20Generate%20New%20Token%20button.) is required to connect to the sql endpoint from this notebook.
+# MAGIC This caching process may not **necessarily** scale the endpoint up, which would also greatly increase reporting performance if done beforehand.
 # MAGIC 
-# MAGIC NOTE2b: We strongly suggest keeping your access token in an [Azure Keyvault backed secret scope](https://docs.microsoft.com/en-us/azure/databricks/security/secrets/secret-scopes).
+# MAGIC #####NOTE2a: 
+# MAGIC 
+# MAGIC A [Databricks Personal Access Token](https://docs.databricks.com/dev-tools/api/latest/authentication.html#:~:text=Generate%20a%20personal%20access%20token,-This%20section%20describes&text=Settings%20in%20the%20lower%20left,the%20Generate%20New%20Token%20button.) is required to connect to the sql endpoint from this notebook.
+# MAGIC 
+# MAGIC #####NOTE2b: 
+# MAGIC 
+# MAGIC We strongly suggest keeping your access token in an [Azure Keyvault backed secret scope](https://docs.microsoft.com/en-us/azure/databricks/security/secrets/secret-scopes).
 
 # COMMAND ----------
 
