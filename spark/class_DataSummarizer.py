3787d974a1364b32988a484a8f3f1b0eb995a8a4
adtullock <78578014+adtullock@users.noreply.github.com>
2022-12-09 08:37:44 -0800

    Update and rename class_DataSummarizer to class_DataSummarizer.py

--- spark/class_DataSummarizer.py
+++ spark/class_DataSummarizer.py
@@ -59,26 +59,40 @@ class SparkDataSummary:
             value_counts.update({name:str(value_count)})
             
         bool_counts = {'T':true_counts, 'F':false_counts, 'ALL':value_counts}
-        return bool_counts    
+        return bool_counts
+        
+    @classmethod
+    def _make_row_template(cls, columns):
+        """Helper function to manually create a row template
+        :param columns (list): Column names to include in the summary"""
+        template = {'summary':""}
+        __ = [template.update({c:""}) for c in columns]
+        
+        return template
+    
+    def _create_new_row(self, title:str, rowTemplate:dict, rowType='count':str):
+        """Helper function to create a new row entry
+        :param title: Title for the metadata row
+        :param rowTemplate: Template containing the columns to compute calculations for
+        :param rowType: Accepts values in (count, percent, distinct)
+        """
+        columns = [x for x in rowTemplate.keys()][1:]
+        new_row_dict = rowTemplate.copy()
+        new_row_dict.update({'summary':title})
+        if rowType == 'count':
+            __ = [new_row_dict.update({c:str(self._null_counts.get(c))}) for c in columns]
+        elif rowType == 'percent':
+                __ = [new_row_dict.update({c:str(100*(self._null_counts.get(c)/self.row_count))}) for c in columns]
+        elif rowType == 'distinct':
+            distinct = lambda x: self._string_df.select(x).distinct().count()
+            __ = [new_row_dict.update({c:str(distinct(c))}) for c in columns]
+        
+        return new_row_dict
     
     def summary(self, dtype='numeric'):
         """Display a profile for all columns of the specified 'dtype'
         :param dtype (str): Accepts these values: "bool", "numeric", "string"
         """
-        def create_new_row(self, title, rowTemplate, rowType='count'):
-            columns = [x for x in rowTemplate.keys()][1:]
-            new_row_dict = rowTemplate.copy()
-            new_row_dict.update({'summary':title})
-            if rowType == 'count':
-                __ = [new_row_dict.update({c:str(self._null_counts.get(c))}) for c in columns]
-            elif rowType == 'percent':
-                __ = [new_row_dict.update({c:str(100*(self._null_counts.get(c)/self.row_count))}) for c in columns]
-            elif rowType == 'distinct':
-                distinct = lambda x: self._string_df.select(x).distinct().count()
-                __ = [new_row_dict.update({c:str(distinct(c))}) for c in columns]
-                
-            return new_row_dict
-        
         df = {
             "bool":self._boolean_df,
             "numeric":self._numeric_df,
@@ -89,8 +103,7 @@ class SparkDataSummary:
         
         if len(row_template.keys()) < 2:
             # Manually make a row template
-            row_template = {'summary':""}
-            __ = [row_template.update({c:""}) for c in df.columns]
+            row_template = self._make_row_template(df.columns)
         
         # Get df columns
         columns = [x for x in row_template.keys()][1:]
@@ -105,15 +118,15 @@ class SparkDataSummary:
         
         if dtype == 'string':
             # Add string column metadata
-            new_row = create_new_row(self, 'distinct', row_template, rowType='distinct')
+            new_row = self._create_new_row('distinct', row_template, rowType='distinct')
             new_rows.append(new_row)
         
         # Add row for null counts
-        new_row = create_new_row(self, 'nulls', row_template)
+        new_row = self._create_new_row('nulls', row_template)
         new_rows.append(new_row)
         
         # Add row for null %
-        new_row = create_new_row(self, 'null%', row_template, rowType='percent')
+        new_row = self._create_new_row('null%', row_template, rowType='percent')
         new_rows.append(new_row)
         
         if dtype == 'bool':
@@ -124,6 +137,31 @@ class SparkDataSummary:
         elif dtype == 'string':
             summary_df = df.summary('count')
             summary_df = summary_df.unionByName(spark.createDataFrame(new_rows))
+        
+        display(summary_df)
+        
+        return None
+    
+    
+    def distinct_value_summary(self):
+        """Display a distinct value counts summary for numeric and string columns
+        """
+        distinct = lambda x: df.select(x).distinct().count()
+        
+        df_columns = self._string_df.columns.copy()
+        df_columns.extend(self._numeric_df.columns)
+        df = self.df.select(df_columns)
+        
+        summary_df = df.summary('count')
+        row_template = summary_df.collect()[0].asDict()
+        
+        new_rows = []
+        new_row = row_template.copy()
+        new_row.update({'summary':'distinct'})
+        __ = [new_row.update({c:str(distinct(c))}) for c in df_columns]
+        new_rows.append(new_row)
+        
+        summary_df = summary_df.unionByName(spark.createDataFrame(new_rows))
         
         display(summary_df)
         
